770c6ad0a5ba65f4f86c72798c0800079299caab
xiaohan2013 <tanyonxiaohan@163.com>
2014-11-25 11:15:18 +0800

    IE兼容CSS3

---
+++ CSSforIE/index.php
@@ -0,0 +1,51 @@
+<?php
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="shihezi">
+    <meta http-equiv="Pragma" contect="no-cache">
+    <title>html demo</title>
+  	<link rel="stylesheet" type="text/css" href="lib/css/style.css"/>
+    <script type="text/javascript" src="lib/js/jquery.min.js"></script>
+
+    <!--[if  lt IE 9]>
+        <script src="lib/js/IE9.js"></script>
+        <script src="lib/js/html5shiv.js"></script>
+        <script src="lib/js/respond.min.js"></script>
+				<script src="lib/js/PIE_IE678.js"></script>
+				<script src="lib/js/selectivizr-min.js"></script>
+    <![endif]-->
+    <!--[if IE 9]>
+    	<script src="lib/js/PIE_IE9.js"></script>
+    	<![endif]-->
+  </head>
+  
+  <body>
+  	<div id="outskirts" class="main-content">
+  	<p class="sdsd"><span>8.9</span>My name is Donald.</p> 
+  	</div>
+<div class="footer-panel-style">
+<div id="footer" class="clear">
+        <ul class="topic-btn-panel ul-horizon">
+          <li><a class="foot-but">BUTTON1</a></li>
+          <li><a href="news.html">BUTTON2</a></li>
+          <li><a href="specials.html">BUTTON3</a></li>
+        </ul>
+      </div>
+</div>
+  
+    <script type="text/javascript">
+        $(document).ready(function()
+        {
+            
+        });
+    </script>
+    <!--[if lt IE 8 ]>
+<script>
+   <script src="lib/js/borderBoxModel-min.js"></script> 
+</script>
+<![endif]-->
+  </body>
+</html>
